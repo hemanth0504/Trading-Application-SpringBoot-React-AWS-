f3d60e64643a9a95bb6b251571d5fa0d5cad8869
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-02 17:15:55 -0400

    Created User Repository

---
+++ src/main/java/com/Hemanth/trading/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.Hemanth.trading.repository;
+
+
+import com.Hemanth.trading.modal.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+// Make User Database changes using this repository
+public interface UserRepository extends JpaRepository<User,Long> {
+
+
+}
