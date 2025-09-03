866ede709f90cd570bb6c0cec2c18ab051a930a7
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-03 19:41:45 -0400

    Auth Controller Changes

--- src/main/java/com/Hemanth/trading/controller/AuthController.java
+++ src/main/java/com/Hemanth/trading/controller/AuthController.java
@@ -1,15 +1,21 @@
 package com.Hemanth.trading.controller;
 
+import com.Hemanth.trading.config.JwtProvider;
 import com.Hemanth.trading.modal.User;
 import com.Hemanth.trading.repository.UserRepository;
+import com.Hemanth.trading.response.AuthResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/auth")
@@ -18,17 +24,48 @@ public class AuthController {
     @Autowired
     private UserRepository userRepository;
 
-@PostMapping("/signup")
-    public ResponseEntity<User> register(@RequestBody User user){
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(
+            @RequestBody User user) throws Exception {
 
-            User newUser = new User();
-            newUser.setEmail(user.getEmail());
-            newUser.setPassword(user.getPassword());
-            newUser.setFullName(user.getFullName());
-                User savedUser = userRepository.save(newUser);
+        String email = user.getEmail();
+        String password = user.getPassword();
+        String fullName = user.getFullName();
+        String mobile=user.getMobile();
 
-                return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+
+        User isEmailExist = userRepository.findByEmail(email);
+
+        if (isEmailExist!=null) {
+
+            throw new Exception("Email Is Already Used With Another Account");
+        }
+
+        // Create new user
+        User createdUser = new User();
+        createdUser.setEmail(email);
+        createdUser.setFullName(fullName);
+        createdUser.setMobile(mobile);
+
+
+        User savedUser = userRepository.save(createdUser);
+
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(email, password);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = JwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(token);
+        authResponse.setMessage("Register Success");
+
+        return new ResponseEntity<AuthResponse>(authResponse, HttpStatus.OK);
+
     }
+
+
+
 
 
 
