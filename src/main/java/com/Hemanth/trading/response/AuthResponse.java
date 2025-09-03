bf4e29410adec2c6e97ee5c794c4c8c8dfb43f7e
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-03 19:42:06 -0400

    Created Auth Response

---
+++ src/main/java/com/Hemanth/trading/response/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.Hemanth.trading.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+public class AuthResponse {
+
+    private String jwt;
+    private boolean status;
+    private String message;
+    private boolean isTwoFactorAuthEnabled=false;
+    private String session;
+
+}
