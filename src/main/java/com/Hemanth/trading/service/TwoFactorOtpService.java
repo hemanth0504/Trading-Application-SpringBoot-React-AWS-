efa58345352fba5837f60af76e9f27978634955c
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-04 17:57:55 -0400

    Implemented Two Factor Otp Service

---
+++ src/main/java/com/Hemanth/trading/service/TwoFactorOtpService.java
@@ -0,0 +1,19 @@
+package com.Hemanth.trading.service;
+
+import com.Hemanth.trading.modal.TwoFactorOTP;
+import com.Hemanth.trading.modal.User;
+
+
+public interface TwoFactorOtpService {
+
+    TwoFactorOTP createTwoFactorOtp(User user, String otp, String jwt);
+
+    TwoFactorOTP findByUser(Long userId);
+
+    TwoFactorOTP findById(String id);
+
+    boolean verifyTwoFactorOtp(TwoFactorOTP twoFactorOtp,String otp);
+
+    void deleteTwoFactorOtp(TwoFactorOTP twoFactorOTP);
+
+}
