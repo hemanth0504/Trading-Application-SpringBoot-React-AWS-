18d52f7d3b2b1805cdae7491b1769b2c772a6272
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-04 17:54:50 -0400

    Signup Auth

--- src/main/java/com/Hemanth/trading/controller/AuthController.java
+++ src/main/java/com/Hemanth/trading/controller/AuthController.java
@@ -4,12 +4,15 @@ import com.Hemanth.trading.config.JwtProvider;
 import com.Hemanth.trading.modal.User;
 import com.Hemanth.trading.repository.UserRepository;
 import com.Hemanth.trading.response.AuthResponse;
+import com.Hemanth.trading.service.CustomeUserServiceImplementation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +26,8 @@ public class AuthController {
 
     @Autowired
     private UserRepository userRepository;
+    private CustomeUserServiceImplementation customUserDetails;
+
 
     @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(
@@ -64,6 +69,45 @@ public class AuthController {
 
     }
 
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signing(@RequestBody LoginRequest loginRequest) throws UserException, MessagingException {
+
+        String username = loginRequest.getEmail();
+        String password = loginRequest.getPassword();
+
+        System.out.println(username + " ----- " + password);
+
+        Authentication authentication = authenticate(username, password);
+
+        User user= userService.findUserByEmail(username);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String token = JwtProvider.generateToken(authentication);
+
+        AuthResponse authResponse = new AuthResponse();
+
+        authResponse.setMessage("Login Success");
+        authResponse.setJwt(token);
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+
+        System.out.println("sign in userDetails - " + userDetails);
+
+        if (userDetails == null) {
+            System.out.println("sign in userDetails - null " + userDetails);
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        if (!password.equals(userDetails.getPassword())) {
+            System.out.println("sign in userDetails - password not match " + userDetails);
+            throw new BadCredentialsException("Invalid username or password");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+    }
 
 
 
