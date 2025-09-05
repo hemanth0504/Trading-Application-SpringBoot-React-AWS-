38e3a4ffdc86889015c64b363b216fede8a5448c
hemanth0504 <hemanth.teegala5@gmail.com>
2025-09-05 19:16:06 -0400

    Asset Service Interface and Implementation

---
+++ src/main/java/com/Hemanth/trading/service/AssetService.java
@@ -0,0 +1,27 @@
+package com.Hemanth.trading.service;
+
+
+
+import com.Hemanth.trading.model.Asset;
+import com.Hemanth.trading.model.Coin;
+import com.Hemanth.trading.model.User;
+
+import java.util.List;
+
+public interface AssetService {
+    Asset createAsset(User user, Coin coin, double quantity);
+
+    Asset getAssetById(Long assetId);
+
+    Asset getAssetByUserAndId(Long userId,Long assetId);
+
+    List<Asset> getUsersAssets(Long userId);
+
+    Asset updateAsset(Long assetId,double quantity) throws Exception;
+
+    Asset findAssetByUserIdAndCoinId(Long userId,String coinId) throws Exception;
+
+    void deleteAsset(Long assetId);
+
+
+}
